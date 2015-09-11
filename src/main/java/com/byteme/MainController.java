823a3cdafae2d8addb5e07613ee13b97f972f552
Siddarth Senthilkumar <ssenthilkumar3@gatech.edu>
2015-09-10 23:36:23 -0400

    Created controller class. Opened player config after game config on OK button press.

---
+++ src/main/java/com/byteme/MainController.java
@@ -0,0 +1,30 @@
+package com.byteme;
+
+import javafx.scene.Parent;
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+/**
+ * Created by Siddarth on 9/10/2015.
+ */
+public class MainController {
+
+    public void openPlayerConfig() throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("fxml/ConfigureScreen.fxml"));
+            Stage stage = new Stage();
+            stage.initModality(Modality.APPLICATION_MODAL);
+            stage.setOpacity(1);
+            stage.setTitle("My New Stage Title");
+            stage.setScene(new Scene(root, 450, 450));
+            stage.showAndWait();
+
+    }
+}
