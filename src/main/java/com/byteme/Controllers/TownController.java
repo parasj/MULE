4477b70aa61e14fc84d8bc5d9a3e203e57af8890
Daniel Kim <dkim6300@gmail.com>
2015-09-30 10:19:44 -0400

    Town Controller with pub

--- src/main/java/com/byteme/Controllers/TownController.java
+++ src/main/java/com/byteme/Controllers/TownController.java
@@ -1,16 +1,30 @@
 package com.byteme.Controllers;
 
+import com.byteme.Models.ConfigRepository;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.shape.Rectangle;
+
+import java.net.URL;
+import java.util.ResourceBundle;
 
 /**
  * Created by Daniel on 9/18/2015.
  */
-public class TownController {
-
+public class TownController implements Initializable{
+    @FXML
+    private Rectangle pub;
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        pub.setOnMouseClicked((MouseEvent e) -> goToPub());
+    }
 
     public void goToMap() {
         MasterController.getInstance().map();
+    }
+
+    public void goToPub() {
+        MasterController.getInstance().pubScene();
     }
 }
