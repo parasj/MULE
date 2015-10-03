473d6335b3b95e1dc49b008774e369cabe21010c
Paras Jain <parasjain@parasjain.com>
2015-10-02 23:47:47 -0400

    Board controller

---
+++ src/main/java/com/byteme/Controllers/BoardController.java
@@ -0,0 +1,17 @@
+package com.byteme.Controllers;
+
+/**
+ * Created by parasjain on 10/2/15.
+ */
+public class BoardController {
+
+    
+    public void passButtonClicked() {
+        log("Pass");
+
+    }
+
+    private void log(String s) {
+        System.out.println(s);
+    }
+}
