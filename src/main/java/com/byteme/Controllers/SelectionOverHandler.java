41ab91c022fdb481906e8f90b581fd61507eb85a
rbose365 <rishavbose365@gmail.com>
2015-10-03 00:01:03 -0400

    Delete Controllers

--- src/main/java/com/byteme/Controllers/SelectionOverHandler.java
+++ src/main/java/com/byteme/Controllers/SelectionOverHandler.java
@@ -3,5 +3,5 @@ package com.byteme.Controllers;
 /**
  * Created by rishav on 10/2/2015.
  */
-public class SelectionOverController extends MapStateController  {
+public class SelectionOverHandler extends MapStateHandler {
 }
