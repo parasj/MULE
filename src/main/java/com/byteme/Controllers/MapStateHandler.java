54cc32446b56884f65388e53fc1d7124cc40393f
rbose365 <rishavbose365@gmail.com>
2015-10-03 00:16:34 -0400

    Add methods to be used in all handlers

--- src/main/java/com/byteme/Controllers/MapStateHandler.java
+++ src/main/java/com/byteme/Controllers/MapStateHandler.java
@@ -4,6 +4,21 @@ package com.byteme.Controllers;
  * Created by rishav on 10/2/2015.
  */
 public abstract class MapStateHandler {
+
+    private BoardController boardController;
     public abstract void handlePass();
-    //Defiine functions necessary here
+    public abstract void handleTileChosen();
+    public abstract void handleTownButtonClicked();
+
+    public MapStateHandler(BoardController boardController) {
+        this.boardController = boardController;
+    }
+
+    public BoardController getBoardController() {
+        return boardController;
+    }
+
+    public void setBoardController(BoardController boardController) {
+        this.boardController = boardController;
+    }
 }
