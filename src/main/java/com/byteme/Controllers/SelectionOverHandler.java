dd6d0bc8d88fcfc610f4230a71ec5169705d003f
Paras Jain <parasjain@parasjain.com>
2015-10-03 01:16:05 -0400

    abc

--- src/main/java/com/byteme/Controllers/SelectionOverHandler.java
+++ src/main/java/com/byteme/Controllers/SelectionOverHandler.java
@@ -1,7 +1,38 @@
 package com.byteme.Controllers;
 
+import javafx.scene.input.MouseEvent;
+
 /**
  * Created by rishav on 10/2/2015.
  */
 public class SelectionOverHandler extends MapStateHandler {
+    @Override
+    public void handlePass() {
+
+    }
+
+    @Override
+    public void handleTileChosen() {
+
+    }
+
+    @Override
+    public void handleTownButtonClicked() {
+
+    }
+
+    @Override
+    public void tileChosen(MouseEvent event) {
+
+    }
+
+    @Override
+    public void stateChanged() {
+
+    }
+
+    @Override
+    public void tick() {
+
+    }
 }
--- src/main/java/com/byteme/Controllers/StartHandler.java
+++ src/main/java/com/byteme/Controllers/StartHandler.java
@@ -1,7 +1,41 @@
 package com.byteme.Controllers;
 
+import javafx.scene.input.MouseEvent;
+
 /**
  * Created by rishav on 10/2/2015.
  */
 public class StartHandler extends MapStateHandler {
+
+    // do nothing
+
+    @Override
+    public void handlePass() {
+
+    }
+
+    @Override
+    public void handleTileChosen() {
+
+    }
+
+    @Override
+    public void handleTownButtonClicked() {
+
+    }
+
+    @Override
+    public void tileChosen(MouseEvent event) {
+
+    }
+
+    @Override
+    public void stateChanged() {
+
+    }
+
+    @Override
+    public void tick() {
+
+    }
 }
