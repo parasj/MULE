874181fa3f6fb34ed3f4ad1aff5753ff031808a4
Paras Jain <parasjain@parasjain.com>
2015-10-02 23:52:17 -0400

    Handle pass function

--- src/main/java/com/byteme/Controllers/BoardController.java
+++ src/main/java/com/byteme/Controllers/BoardController.java
@@ -1,14 +1,18 @@
 package com.byteme.Controllers;
 
+import com.byteme.Models.MapStateStore;
+
 /**
  * Created by parasjain on 10/2/15.
  */
 public class BoardController {
+    private MapStateController childController;
+    private final MapStateStore s = MapStateStore.getInstance();
+    private 
 
-    
     public void passButtonClicked() {
         log("Pass");
-
+        childController.handlePass();
     }
 
     private void log(String s) {
--- src/main/java/com/byteme/Controllers/MapStateController.java
+++ src/main/java/com/byteme/Controllers/MapStateController.java
@@ -4,5 +4,6 @@ package com.byteme.Controllers;
  * Created by rishav on 10/2/2015.
  */
 public abstract class MapStateController {
+    public abstract void handlePass();
     //Definine functions necessary here
 }
