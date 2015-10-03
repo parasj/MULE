263f4fa397b132ee3eb91af0e947debc017ffb88
rbose365 <rishavbose365@gmail.com>
2015-10-03 03:02:49 -0400

    Clean

--- src/main/java/com/byteme/Controllers/LandGrantHandler.java
+++ src/main/java/com/byteme/Controllers/LandGrantHandler.java
@@ -23,32 +23,6 @@ public class LandGrantHandler extends MapStateHandler {
     public void handlePass() {
         s.incrPlayer();
         checkIfDone();
-//        getBoardController().getAlertsLabel().setVisible(false);
-//        if (s.getCurrentState() == MapControllerStates.LAND_PURCHASE) {
-//            s.setPurchaseOpportunities(s.getPurchaseOpportunities() + 1);
-//        }
-//        s.setPassCounter(s.getPassCounter() + 1);
-//        if (s.getPassCounter() >= s.getNumPlayers()) {
-//            s.setPassCounter(0);
-//            if (s.getCurrentState() == MapControllerStates.LAND_GRANT || s.getCurrentState() == MapControllerStates.LAND_PURCHASE || s.getCurrentState() == MapControllerStates.START)
-//                s.setCurrentState(MapControllerStates.GAME_START);
-//            else
-//                s.setCurrentState(MapControllerStates.GAME_START);
-//            getBoardController().getPhaseLabel().setText("Selection phase is over!");
-//            changePlayer(1);
-//            s.setTimeLeft(pubController.calcTimeLeft(null));
-//            getBoardController().updateState(MapControllerStates.LAND_PURCHASE);
-//        }
-////        Not used in Land Grant Stage
-////        } else {
-////            if (s.getCurrentState() == MapControllerStates.GAME_START || s.getCurrentState() == MapControllerStates.SELECTION_OVER)
-////                pubController.goToMap();
-////            else {
-////                s.getInstance().setTimeLeft(0);
-////                changePlayer();
-////                MasterController.getInstance().map();
-////            }
-////        }
     }
 
     @Override
