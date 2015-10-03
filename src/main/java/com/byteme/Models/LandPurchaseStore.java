5e1c4fa8c781c629a8e7cd19898aa4819f00d59a
rbose365 <rishavbose365@gmail.com>
2015-10-03 02:48:28 -0400

    Make store

---
+++ src/main/java/com/byteme/Models/LandPurchaseStore.java
@@ -0,0 +1,51 @@
+package com.byteme.Models;
+
+import com.byteme.Schema.PlayerConfigParams;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by rishav on 10/3/2015.
+ */
+public class LandPurchaseStore {
+    private final static ConfigRepository configRepository = ConfigRepository.getInstance();
+    private static LandPurchaseStore ourInstance = new LandPurchaseStore();
+
+    private List<PlayerConfigParams> players;
+    private int currentPlayer;
+    private int currentPropertyCount;
+
+    public static LandPurchaseStore getInstance() {
+        return ourInstance;
+    }
+
+    private LandPurchaseStore() {
+        players = new ArrayList<PlayerConfigParams>(configRepository.getPlayers());
+        currentPlayer = 0;
+        currentPropertyCount = 0;
+    }
+
+    public void incrPlayer() {
+        if (currentPlayer == players.size() - 1) {
+            incrPropertyCount();
+        }
+        currentPlayer = (currentPlayer + 1) % players.size();
+    }
+
+    public PlayerConfigParams getCurrentPlayer() {
+        return players.get(currentPlayer);
+    }
+
+    public void incrPropertyCount() {
+        currentPropertyCount++;
+    }
+
+    public int getCurrentPropertyCount() {
+        return currentPropertyCount;
+    }
+
+    public int getCurrentPlayerId() {
+        return currentPlayer;
+    }
+}
