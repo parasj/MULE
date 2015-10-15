98a3f0adddb6b864be33d32146bfadc431922807
Daniel Kim <dkim6300@gmail.com>
2015-10-14 22:05:50 -0400

    Added Music

--- src/main/java/com/byteme/Main.java
+++ src/main/java/com/byteme/Main.java
@@ -2,9 +2,12 @@ package com.byteme;
 
 import com.byteme.Controllers.MasterController;
 import javafx.application.Application;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.net.URL;
 
 public class Main extends Application {
 
@@ -18,7 +21,10 @@ public class Main extends Application {
         MasterController controller = MasterController.getInstance();
         controller.setStage(stage);
         controller.startGame();
-
+        URL resource = getClass().getResource("/music/stupidblues01.mp3");
+        Media media = new Media(resource.toString());
+        MediaPlayer mediaPlayer = new MediaPlayer(media);
+        mediaPlayer.play();
         stage.setTitle("M.U.L.E. - ByteMe");
         stage.setResizable(false);
         stage.show();
