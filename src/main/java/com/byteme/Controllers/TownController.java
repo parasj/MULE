576ba8adc2e6e16c73ca5cd36ae6bc89cce2fe4e
rbose365 <rishavbose365@gmail.com>
2015-11-02 15:31:32 -0500

    Add comments to TownController

--- src/main/java/com/byteme/Controllers/TownController.java
+++ src/main/java/com/byteme/Controllers/TownController.java
@@ -7,15 +7,18 @@ import com.byteme.Schema.MapControllerStates;
  */
 public class TownController {
 
+    //goes to map when map square clicked
     public void goToMap() {
         MasterController.getInstance().map();
     }
 
+    //Similar
     public void goToPub() {
         MasterController.getInstance().getBoardController().updateState(MapControllerStates.TURN_OVER, true);
         MasterController.getInstance().pubScene();
     }
 
+    //Similar
     public void goToStore() {
         MasterController.getInstance().store();
     }
