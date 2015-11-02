b363135df288bf3eb1b2950555154970828b7b80
rbose365 <rishavbose365@gmail.com>
2015-11-02 15:11:03 -0500

    Add comments to MapStateHandler

--- src/main/java/com/byteme/Controllers/MapStateHandler.java
+++ src/main/java/com/byteme/Controllers/MapStateHandler.java
@@ -1,7 +1,5 @@
 package com.byteme.Controllers;
 
-import com.byteme.Models.MULEStore;
-import com.byteme.Models.MapStateStore;
 import com.byteme.Util.CanTick;
 import javafx.fxml.Initializable;
 import javafx.scene.input.MouseEvent;
@@ -9,14 +7,13 @@ import javafx.scene.input.MouseEvent;
 /**
  * MULE
  */
+//Every handler inherits this
 public abstract class MapStateHandler implements CanTick, Initializable {
 
     private BoardController boardController;
-    private MapStateStore s;
 
     public MapStateHandler(BoardController boardController) {
         this.boardController = boardController;
-        s = MULEStore.getInstance().getMapStateStore();
     }
 
     public BoardController getBoardController() {
@@ -38,29 +35,4 @@ public abstract class MapStateHandler implements CanTick, Initializable {
     protected void log(String log) {
         System.out.println(log);
     }
-
-
-//    Implement in each state store
-//    /**
-//     * Updates the player label to next player's name.
-//     * Increments currentPlayer.
-//     */
-//    public void changePlayer() {
-//        if (s.getCurrentPlayer() + 1 == s.getNumPlayers()) {
-//            changePlayer(s.getNumPlayers());
-//        } else {
-//            changePlayer((s.getCurrentPlayer() + 1) % s.getNumPlayers());
-//        }
-//    }
-//
-//    /**
-//     * Updates the player label to next player's name.
-//     * Increments currentPlayer.
-//     * @param playerNumber The number of the player to be set
-//     */
-//    public void changePlayer(int playerNumber) {
-//        s.setCurrentPlayer(playerNumber);
-//        //rerenderPlayerText();
-//    }
-
 }
