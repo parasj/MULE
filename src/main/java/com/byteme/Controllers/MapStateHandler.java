1e5fc3fcbbc004f71689ee9f274d17d5e130cd25
rbose365 <rishavbose365@gmail.com>
2015-11-03 16:30:58 -0500

    Javadoc Template

--- src/main/java/com/byteme/Controllers/MapStateHandler.java
+++ src/main/java/com/byteme/Controllers/MapStateHandler.java
@@ -12,26 +12,55 @@ public abstract class MapStateHandler implements CanTick, Initializable {
 
     private BoardController boardController;
 
+    /**
+     *
+     * @param boardController
+     */
     public MapStateHandler(BoardController boardController) {
         this.boardController = boardController;
     }
 
+    /**
+     *
+     * @return
+     */
     public BoardController getBoardController() {
         return boardController;
     }
 
+    /**
+     *
+     * @param boardController
+     */
     public void setBoardController(BoardController boardController) {
         this.boardController = boardController;
     }
 
+    /**
+     *
+     */
     public abstract void handlePass();
 
+    /**
+     *
+     */
     public abstract void handleTownButtonClicked();
 
+    /**
+     *
+     * @param event
+     */
     public abstract void tileChosen(MouseEvent event);
 
+    /**
+     *
+     */
     public abstract void stateChanged();
 
+    /**
+     *
+     * @param log
+     */
     protected void log(String log) {
         System.out.println(log);
     }
