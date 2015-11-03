b604da84676de6d197d57e87ff7a2ff13d63c915
rbose365 <rishavbose365@gmail.com>
2015-11-03 16:55:03 -0500

    Javadoc Template

--- src/main/java/com/byteme/Models/ConfigRepository.java
+++ src/main/java/com/byteme/Models/ConfigRepository.java
@@ -14,32 +14,79 @@ public class ConfigRepository implements Serializable {
     private GameConfigParams gameConfigParams;
     private Map<Integer, PlayerConfigParams> playerConfigList;
 
+    /**
+     *
+     */
     public ConfigRepository() {
         playerConfigList = new HashMap<>(3);
     }
 
     // Getters and Setters
+
+    /**
+     *
+     * @return
+     */
     public GameConfigParams getGameConfig() {
         return gameConfigParams;
     }
+
+    /**
+     *
+     * @param config
+     */
     public void setGameConfig(GameConfigParams config) {
-        gameConfigParams = config;
+        if (gameConfigParams != null) {
+            gameConfigParams = config;
+        }
     }
+
+    /**
+     *
+     * @return
+     */
     public int getTotalPlayers() {
         return gameConfigParams.getNumPlayers();
     }
+
+    /**
+     *
+     * @param id
+     * @return
+     */
     public PlayerConfigParams getPlayerConfig(int id) {
         return playerConfigList.get(id);
     }
+
+    /**
+     *
+     * @param player
+     * @param id
+     */
     public void setPlayerConfig(PlayerConfigParams player, int id) {
-        playerConfigList.put(id, player);
+        if (player != null) {
+            playerConfigList.put(id, player);
+        }
     }
+
+    /**
+     *
+     * @return
+     */
     public Collection<PlayerConfigParams> getPlayers() {
         return playerConfigList.values();
     }
+
+    /**
+     *
+     * @return
+     */
     public PlayerConfigParams getFirstPlayerConfig() {
         return playerConfigList.get(1);
     }
 
+    /**
+     *
+     */
     public void reinit() {}
 }
