dc3c1d414bbe312c443ec97d9a89c9fbf9dda42e
rbose365 <rishavbose365@gmail.com>
2015-11-03 16:23:39 -0500

    Javadoc template for empty handler

--- src/main/java/com/byteme/Controllers/EmptyHandler.java
+++ src/main/java/com/byteme/Controllers/EmptyHandler.java
@@ -6,35 +6,60 @@ import java.net.URL;
 import java.util.ResourceBundle;
 
 public class EmptyHandler extends MapStateHandler {
+    /**
+     *
+     * @param boardController
+     */
     public EmptyHandler(BoardController boardController) {
         super(boardController);
     }
 
+    /**
+     *
+     */
     @Override
     public void handlePass() {
 
     }
 
+    /**
+     *
+     */
     @Override
     public void handleTownButtonClicked() {
 
     }
 
+    /**
+     *
+     * @param event
+     */
     @Override
     public void tileChosen(MouseEvent event) {
 
     }
 
+    /**
+     *
+     */
     @Override
     public void stateChanged() {
 
     }
 
+    /**
+     *
+     */
     @Override
     public void tick() {
 
     }
 
+    /**
+     * 
+     * @param location
+     * @param resources
+     */
     @Override
     public void initialize(URL location, ResourceBundle resources) {
 
