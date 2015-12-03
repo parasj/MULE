af2e732d3cac1eab2d13135d6342cecf7a5b97f7
Paras Jain <parasjain@parasjain.com>
2015-12-02 21:53:19 -0500

    More races in Enum, better storage of money values

--- src/main/java/com/byteme/Schema/Race.java
+++ src/main/java/com/byteme/Schema/Race.java
@@ -4,7 +4,30 @@ package com.byteme.Schema;
  * MULE Race enum.
  */
 public enum Race {
-    FLAPPER, HUMAN, BONZOID, UGAITE, BUZZITE;
+    FLAPPER, HUMAN, BONZOID, UGAITE, BUZZITE,
+    MARTIAN, PLUTONIAN, ORC, DWARF, ATLANTIAN;
+
+    private int startingMoney;
+
+    static {
+        FLAPPER.startingMoney = 1600;
+        HUMAN.startingMoney = 600;
+        BONZOID.startingMoney = 1000;
+        UGAITE.startingMoney = 1000;
+        BUZZITE.startingMoney = 1000;
+
+        // extra races
+        MARTIAN.startingMoney = 1100;
+        PLUTONIAN.startingMoney = 1200;
+        ORC.startingMoney = 1300;
+        DWARF.startingMoney = 1400;
+        ATLANTIAN.startingMoney = 1500;
+
+    }
+
+    public int getStartingMoney() {
+        return startingMoney;
+    }
 
     @Override
     public String toString() {
